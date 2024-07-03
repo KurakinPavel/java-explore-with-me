7c913d2f891cf54b564cd0867b172f21e35b9cd6
KurakinPavel <pu.kurakin@yandex.ru>
2024-07-03 14:48:16 +0300

    Diploma: review 1

--- stats-server/stats-server-client/src/main/java/ru/practicum/statserverclient/client/StatsServerClient.java
+++ stats-server/stats-server-client/src/main/java/ru/practicum/statserverclient/client/StatsServerClient.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.statsserverdto.dto.HitDto;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
